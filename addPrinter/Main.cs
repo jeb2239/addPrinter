06db0e1bbaaffd6f0295133dd1e7527658168531
jeb2239 <jeb2239@columbia.edu>
2014-12-29 21:40:57 -0500

    namspace with two wrapper classes around WMI objects

---
+++ addPrinter/Main.cs
@@ -0,0 +1,15 @@
+﻿using System.Management;
+using System.IO;
+using System.Linq;
+using System;
+using addPrinters;
+
+
+class ninjaPrinters
+{
+    public static void Main()
+    {
+        
+        Console.ReadKey();
+    }
+}
