de8793d90c53fd3bcaff12e6ea189b5071866bc1
jeb2239 <jeb2239@columbia.edu>
2015-01-04 23:45:22 -0500

    figured how to read file from web in C#

--- addPrinter/Main.cs
+++ addPrinter/Main.cs
@@ -2,6 +2,11 @@
 using System.IO;
 using System.Linq;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+
 using addPrinters;
 
 
@@ -9,7 +14,57 @@ class ninjaPrinters
 {
     public static void Main()
     {
+
+       
+
+       Console.ReadKey();
+    }
+
+
+    public static void checkPrinterDb()
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>(); //hash table for fast query
+        String[] links = {"http://www.columbia.edu/acis/facilities/printers/ninja/acis/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/barnard/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/cait/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/lso/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/meche/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/old_pcd/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/soa/js/printers.js",
+"http://www.columbia.edu/acis/facilities/printers/ninja/ssw/js/printers.js"};
+
+        foreach (string link in links)
+        {
+            StreamReader sr = openURL(link);
+            while (!sr.EndOfStream)
+            {
+                string anEntry=sr.ReadLine();
+                if (anEntry.StartsWith("//")|| anEntry.Length<3) //i can't believe this has a startsWith method!
+                {
+                    continue; //skip comments and empty lines
+                }
+
+                
+
+
+            }
+            
+            
+
+
+        }
         
-        Console.ReadKey();
+        
+        
+        
+
+    }
+
+    public static StreamReader openURL(String str)
+    {
+        WebClient client = new WebClient();
+        Stream stream = client.OpenRead(str);
+        StreamReader reader = new StreamReader(stream);
+        return reader;
     }
 }
--- addPrinter/addPrinters.cs
+++ addPrinter/addPrinters.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Management;
 using System.ComponentModel;
+using System.Collections;
 
 namespace addPrinters
 {
